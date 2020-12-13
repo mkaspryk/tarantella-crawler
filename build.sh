7f4335aa88da1f55da1e64ed82dfe8dd81030f40
mkaspryk <mkaspryk09@outlook.com>
2020-12-13 22:16:27 +0100

    build file - 13.12.2020

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+#============================================
+# Name          :   build.sh
+# Author        :   Marcin Grzegorz Kaspryk
+# Version       :   1.0.0
+# Copyright     :   ASL
+# Description   :   Build script
+#============================================
+
+echo "Hello there"
