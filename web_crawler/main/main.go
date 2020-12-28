b4b89331aaf0c6bd167db69e7df602702e09c1e2
mkaspryk <mkaspryk09@outlook.com>
2020-12-28 23:04:15 +0100

    web_crawler - 28.12.20

--- web_crawler/main/main.go
+++ web_crawler/main/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"strconv"
@@ -11,20 +10,19 @@ import (
 
 func main() {
 
-	fmt.Println("Hello")
-	if len(os.Args) < 4 {
+	if len(os.Args) < 5 {
 		os.Exit(-1)
 	}
 
 	pageLang := os.Args[1]
 	searchLength := os.Args[2]
 	startPage := os.Args[3]
+	path := os.Args[4]
 
 	sl, err := strconv.Atoi(searchLength)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	scraping.Crawl(pageLang, sl, startPage)
-
+	scraping.Crawl(pageLang, sl, startPage, path)
 }
--- web_crawler/scraping/scraping.go
+++ web_crawler/scraping/scraping.go
@@ -1,51 +1,63 @@
 package scraping
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
 type webPages struct {
-	crawled      map[string]bool
+	wordsCount   []int
 	urls         []string
-	clearContent []string
+	clearContent [][]string
 	pageNumber   int
 	searchLength int
 	pageLang     string
+	pageName     []string
 }
 
 // webPages constructor
 func new(searchLength int, pageLang string) *webPages {
 
 	return &webPages{
-		crawled:      make(map[string]bool),
 		searchLength: searchLength,
 		pageNumber:   0,
 		pageLang:     pageLang,
+		clearContent: make([][]string, searchLength),
 	}
 }
 
 // Crawl performs url and text content search
-func Crawl(pageLang string, searchLength int, startPage string) {
+func Crawl(pageLang string, searchLength int, startPage string, path string) {
 
 	wb := new(searchLength, pageLang)
 	wb.urls = append(wb.urls, startPage)
 
 	for i := 0; i < wb.searchLength; i++ {
+
+		if wb.pageNumber < i {
+			break
+		}
+
 		doc := getPageContent(wb.urls[i])
-		checkLang(doc, wb.pageLang)
-		getLinksFromPage(doc, *wb)
+		//fmt.Println(wb.urls[i])
+
+		if !checkLang(doc, wb.pageLang) {
+			continue
+		}
+		pageName := pageURL(wb.urls[i], &*wb)
+		getLinksFromPage(doc, &*wb, pageName)
+		pageText := strings.ToLower(doc.Text())
+		scrap(&*wb, pageText, i)
+		//for j := 0; j < wb.wordsCount[i]; j++ {
+		//	fmt.Println(wb.clearContent[i][j])
+		//}
 	}
-}
-
-// Scraping the data from page
-func scraping(pageContent string) (err error) {
-
-	fmt.Println("here")
-	return err
+	saveToFile(&*wb, path)
 }
 
 // GetPageContent gets the page content (urls, text)
@@ -61,7 +73,7 @@ func getPageContent(url string) goquery.Document {
 		log.Fatalf("status code error: %d %s", resp.StatusCode, resp.Status)
 	}
 
-	// Load the HTML document
+	// Load the HTML document with goquery
 	doc, err := goquery.NewDocumentFromReader(resp.Body)
 	if err != nil {
 		log.Fatal(err)
@@ -69,23 +81,155 @@ func getPageContent(url string) goquery.Document {
 	return *doc
 }
 
+// checkLang checks the lang of page
 func checkLang(doc goquery.Document, pageLang string) bool {
-	return false
+
+	found := false
+	doc.Find("html").Each(func(index int, item *goquery.Selection) {
+		if item.AttrOr("lang", "") == pageLang {
+			found = true
+		}
+	})
+	return found
 }
 
-func getLinksFromPage(doc goquery.Document, wb webPages) {
+// pageURL returns website name
+func pageURL(url string, wb *webPages) string {
+
+	found := 0
+	tmp := ""
+	for i := 0; i < len(url); i++ {
+
+		if url[i] == 47 {
+			found++
+		}
+		if found == 3 {
+			for j := 0; j < i; j++ {
+				tmp += string(url[j])
+			}
+			break
+		}
+	}
+	wb.pageName = append(wb.pageName, tmp)
+	return tmp
+}
+
+// getLinksFromPage gets the links from page
+func getLinksFromPage(doc goquery.Document, wb *webPages, pageURL string) {
 
 	doc.Find("body a").EachWithBreak(func(index int, item *goquery.Selection) bool {
 		if index == 0 {
 			return true
 		}
-		wb.pageNumber = index
+		wb.pageNumber++
 		if wb.pageNumber <= wb.searchLength {
 			linkTag := item
 			link, _ := linkTag.Attr("href")
-			wb.urls = append(wb.urls, link)
+			if link[0] == 35 {
+				wb.pageNumber--
+			} else {
+				found := false
+				for j := 0; j < wb.pageNumber; j++ {
+					if wb.urls[j] == link {
+						found = true
+						break
+					}
+				}
+				if !found {
+					if link[0] == 47 {
+						link = pageURL + link
+					}
+					wb.urls = append(wb.urls, link)
+				}
+			}
 			return true
 		}
 		return false
 	})
+}
+
+// scrap clears the text content and splits the words
+func scrap(wb *webPages, pageText string, index int) {
+
+	wordsIndex := 0
+	save := false
+	wordLong := 0
+	var tmp string
+	for i := 0; i < len(pageText); i++ {
+		if pageText[i] > 96 && pageText[i] < 123 || pageText[i] == 45 || pageText[i] == 39 && wordLong < 46 {
+			tmp += string(pageText[i])
+			wordLong++
+			save = true
+		} else if save {
+			save = false
+			wb.clearContent[index] = append(wb.clearContent[index], tmp)
+			tmp = ""
+			wordLong = 0
+			wordsIndex++
+		}
+	}
+	wb.wordsCount = append(wb.wordsCount, wordsIndex)
+}
+
+func saveToFile(wb *webPages, path string) {
+
+	count := ""
+	pageNames := ""
+	pageFiles := ""
+
+	for i := 0; i < wb.searchLength; i++ {
+
+		count += strconv.Itoa(wb.wordsCount[i])
+		count += "\n"
+		pageNames += wb.pageName[i]
+		pageNames += "\n"
+		pageFiles += wb.urls[i]
+		pageFiles += "\n"
+
+		pageWords := ""
+		for j := 0; j < wb.wordsCount[i]; j++ {
+			pageWords += wb.clearContent[i][j]
+			pageWords += "\n"
+		}
+
+		f, err := os.Create(path + strconv.Itoa(i))
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		f.WriteString(pageWords)
+
+		f.Close()
+	}
+
+	f1, err1 := os.Create(path + "count.txt")
+	if err1 != nil {
+		log.Fatal(err1)
+		return
+	}
+
+	f1.WriteString(count)
+
+	f1.Close()
+
+	f2, err2 := os.Create(path + "pageNames.txt")
+	if err2 != nil {
+		log.Fatal(err2)
+		return
+	}
+
+	f2.WriteString(pageNames)
+
+	f2.Close()
+
+	f3, err3 := os.Create(path + "pageFiles.txt")
+	if err3 != nil {
+		log.Fatal(err3)
+		return
+	}
+
+	f3.WriteString(pageFiles)
+
+	f3.Close()
 }
