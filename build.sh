d1474b56ecce240eb56927666a4a2b289262e9bb
mkaspryk <mkaspryk09@outlook.com>
2020-12-28 23:04:56 +0100

    build script - 28.12.20

--- build.sh
+++ build.sh
@@ -8,4 +8,30 @@
 # Description   :   Build script
 #============================================
 
-echo "Hello there"
+path="/home/mgk/repo/go/src/github.com/tarantella-crawler"
+
+word_finder=$path/word_finder/
+
+local_device_finder=$path/local_device_finder/cuda_device_finder/
+
+web_crawler=$path/web_crawler/main/
+
+echo "Building tarantella-crawler..."
+
+cd $word_finder 
+
+make clean
+
+make -j4
+
+cd $local_device_finder 
+
+make clean
+
+make -j4
+
+cd $web_crawler
+
+make clean
+
+make build -j4
