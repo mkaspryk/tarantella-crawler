97308195440c76d890f6fd80f9ad7d8a1659ef97
mkaspryk <mkaspryk09@outlook.com>
2020-12-30 17:21:50 +0100

    tarantella-crawler - 30.12.20

--- tarantella-crawler.sh
+++ tarantella-crawler.sh
@@ -3,7 +3,7 @@
 #============================================
 # Name          :   tarantella-crawler.sh
 # Author        :   Marcin Grzegorz Kaspryk
-# Version       :   1.0.0
+# Version       :   1.1.1
 # Copyright     :   ASL
 # Description   :   Main program
 #============================================
@@ -11,6 +11,8 @@
 echo "==============================================================="
 echo "                      tarantella-crawler                       "
 echo "==============================================================="
+
+package="tarantella crawler"
 
 path=$(pwd)
 
@@ -22,10 +24,73 @@ results=$path/data/results
 lang="en"
 searchLength=10
 startPage="https://en.wikipedia.org/wiki/Ski_jumping"
+tableName="data"
+
+while test $# -gt 0; do
+  case "$1" in
+    -h|--help)
+      echo "$package - detects offensive language at the web pages"
+      echo " "
+      echo "$package [options] application [arguments]"
+      echo " "
+      echo "options:"
+      echo "-h, --help                show brief help"
+      echo "-l, --lang                specifies a language of searched words"
+      echo "-p, --page                specifies a start page (URL only starting with https:// or http://)"
+      echo "-s, --search              specifies a search length (numbers of pages to visit)"
+      echo "-t, --table               specifies an output table in pages_data database"
+      exit 0
+      ;;
+    -l|--lang)
+      shift
+      if test $# -gt 0; then
+        export lang=$1
+      else
+        echo "No language specified"
+        exit 1
+      fi
+      shift
+      ;;
+    -p|--page)
+      shift
+      if test $# -gt 0; then
+        export startPage=$1
+      else
+        echo "No page specified"
+        exit 1
+      fi
+      shift
+      ;;
+    -s|--searchLength)
+      shift
+      if test $# -gt 0; then
+        export searchLength=$1
+      else
+        echo "No search length specified"
+        exit 1
+      fi
+      shift
+      ;;
+    -t|--table)
+      shift
+      if test $# -gt 0; then
+        export tableName=$1
+      else
+        echo "No table specified"
+        exit 1
+      fi
+      shift
+      ;;
+    *)
+      break
+      ;;
+  esac
+done
 
 echo "Setted lang: $lang"
 echo "Setted search length: $searchLength"
 echo "Setted start page: $startPage"
+echo "Setted table: $tableName"
 
 CUDA_DEVICE=$path/bin/./cuda_device_finder
 WORD_FINDER=$path/bin/./word_finder
@@ -57,7 +122,7 @@ echo "Running word_finder"
 echo "..."
 
 dic_size=1383
-table_name="data"
+
 
 x=0
 while IFS= read -r size
@@ -79,14 +144,16 @@ do
     done
 done < $formatted_web_content/count.txt
 
+echo ""
+
 # creates table in database
 echo "Creating table"
 echo "..."
-#$CREATE $table_name $dictionary
+$CREATE $tableName $dictionary
 
 echo "Inserting data to table"
 echo "..."
 #inserts data to table in database
-#$INSERT $TABLE_NAME $results
+$INSERT $tableName $results $formatted_web_content
 
 rm -r $results/*
